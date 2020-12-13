cc20a979bd059850e18f503cd9bfbc4d3c180e2d
mecinas <szymon.mecina@vp.pl>
2020-12-13 17:47:25 +0100

    Dividing app into web service and client

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, make_response, session, render_template, flash, url_for, g
-from flask_session import Session
+from flask import Flask, request, make_response, render_template, flash, url_for, g
+import requests
 from redis import Redis
 from os import getenv
 from dotenv import load_dotenv
@@ -13,8 +13,6 @@ db = Redis(host='redis', port='6379', db=0)
 load_dotenv()
 SESSION_TYPE = 'filesystem'
 SESSION_REDIS = db
-# SESSION_COOKIE_SECURE = True
-# REMEMBER_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 REMEMBER_COOKIE_HTTPONLY = True
 JWT_SECRET = getenv("JWT_SECRET")
@@ -22,137 +20,95 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = getenv('SECRET_KEY')
 app.debug = False
-ses = Session(app)
+WEB_SEVICE_URL = "https://murmuring-springs-10121.herokuapp.com"
 
-@app.before_request
-def get_logged_login():
-    g.login = session.get("login")
+def get_respond_render(url, html_resource, cookie=None):
+    api_response = requests.get(url)
+    api_response_data = json.loads(api_response.content)
+    client_response = make_response(render_template(html_resource))
+    client_response.headers.set('links', api_response_data["links"])
+    if cookie is not None:
+        client_response.set_cookie('auth', cookie, max_age=20)
+    return client_response
 
-
-def is_user(username):
-    return db.hexists(f"user:{username}", "password")
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def open_home():
-    return render_template("home.html")
-
+    url = WEB_SEVICE_URL + "/root"
+    return get_respond_render(url, "home.html")
 
 @app.route("/sender/register", methods=['GET'])
 def open_register():
-    return render_template("register.html")
-
-
-def redirect(url, status=301):
-    response = make_response('', status)
-    response.headers['Location'] = url
-    return response
-
-
-def is_login(login):
-    return db.hexists(f"user:{login}", "password")
-
-
-def verify_user(login, password):
-    password_encoded = password.encode()
-    hashed_password_database = db.hget(f"user:{login}", "password")
-    if not hashed_password_database:
-        return False
-    return checkpw(password_encoded, hashed_password_database)
-
-
-def save_user(firstname, lastname, email, adress, login, password):
-    salt = gensalt(5)
-    password_encoded = password.encode()
-    hashed_password = hashpw(password_encoded, salt)
-    db.hset(f"user:{login}", "firstname", firstname)
-    db.hset(f"user:{login}", "lastname", lastname)
-    db.hset(f"user:{login}", "email", email)
-    db.hset(f"user:{login}", "adress", adress)
-    db.hset(f"user:{login}", "password", hashed_password)
-
-
-def validate_input(firstname, lastname, email, adress, login, password, sec_password):
-    if not firstname:
-        flash("Nie wpisano imienia!")
-    if not lastname:
-        flash("Nie wpisano nazwiska!")
-    if not email:
-        flash("Nie wpisano email-a!")
-    if not adress:
-        flash("Nie wpisano adresu!")
-    if not login:
-        flash("Nie wpisano loginu!")
-    if not password:
-        flash("Nie wpisano hasła!")
-    if sec_password != password:
-        flash("Wpisane hasła nie zgadzają się!")
-        return redirect(url_for('open_register'))
-    if firstname and lastname and email and adress and login and password:
-        if is_login(login):
-            flash("Podany login już istnieje!")
-            return redirect(url_for('open_register'))
-        save_user(firstname, lastname, email, adress, login, password)
-        flash("Poprawnie zarejestrowano!")
-        return redirect(url_for('open_login'))
-
-    return redirect(url_for('open_register'))
-
-
-@app.route("/sender/register", methods=['POST'])
-def register():
-    firstname = request.form.get('firstname')
-    lastname = request.form.get('lastname')
-    email = request.form.get('email')
-    adress = request.form.get('adress')
-    login = request.form.get('login')
-    password = request.form.get('password')
-    sec_password = request.form.get('sec_password')
-
-    return validate_input(firstname, lastname, email, adress,
-                          login, password, sec_password)
-
+    url = WEB_SEVICE_URL + "/root/sender/register"
+    return get_respond_render(url, "register.html")
 
 @app.route("/sender/login", methods=['GET'])
 def open_login():
-    return render_template("login.html")
-
-
-@app.route("/sender/login", methods=['POST'])
-def login():
-    login = request.form.get("login")
-    password = request.form.get("password")
-
-    if not verify_user(login, password):
-        flash("Podano nieprawidłowy login lub hasło")
-        return render_template("login.html")
-
-    flash("Zalogowano!")
-    session["login"] = login
-    session[login] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    return render_template("login.html")
-
-
-@app.route("/sender/logout", methods=['GET'])
-def open_logout():
-    session.clear()
-    flash("Wylogowano!")
-    return render_template("login.html")
-
+    url = WEB_SEVICE_URL + "/root/sender/login"
+    return get_respond_render(url, "login.html")
 
 @app.route("/sender/dashboard", methods=['GET'])
 def open_dashboard():
-    login = g.login
-    if login is None:
+    url = WEB_SEVICE_URL + "/root/sender/dashboard"
+
+    api_response = requests.get(url, json=request.cookies.get('auth'))
+    api_response_data = json.loads(api_response.content)
+    return render_dashboard(api_response_data["data"], api_response_data["links"], "dashboard.html", request.cookies.get('auth'))
+
+@app.route("/sender/logout", methods=['GET'])
+def open_logout():
+    url = WEB_SEVICE_URL + "/root/sender/login"
+    api_response = requests.get(url)
+    api_response_data = json.loads(api_response.content)
+    client_response = make_response(render_template("login.html"))
+    client_response.headers.set('links', api_response_data["links"])
+    client_response.delete_cookie('auth')
+    flash("Wylogowano!")
+    return client_response
+
+@app.route("/sender/register", methods=['POST'])
+def register():
+    data = {
+        "firstname": request.form.get('firstname'),
+        "lastname": request.form.get('lastname'),
+        "email": request.form.get('email'),
+        "adress": request.form.get('adress'),
+        "login": request.form.get('login'),
+        "password": request.form.get('password'),
+        "sec_password": request.form.get('sec_password'),
+    }
+
+    url = WEB_SEVICE_URL + "/root/sender/register"
+    api_response = requests.post(url, json=data)
+    api_data = json.loads(api_response.content)
+    for message in api_data["data"]["messages"]:
+        flash(message)
+    return get_respond_render(url,"register.html")
+
+@app.route("/sender/login", methods=['POST'])
+def login():
+    data = {
+        "login": request.form.get("login"),
+        "password": request.form.get("password")
+    }
+    url = WEB_SEVICE_URL + "/root/sender/login"
+    api_response = requests.post(url, json=data)
+    api_data = json.loads(api_response.content)
+    api_cookie = bytes(api_data["data"]["cookies"], 'utf-8')
+
+    return get_respond_render(url, "login.html", cookie = api_cookie)
+
+def render_dashboard(api_response_data, links, html_resource, cookie):
+    if not api_response_data["is_authorized"]:
         return "Brak dostępu, aby przejść do tego panelu należy uprzednio się zalogować", 401
-
-    packages = db.hget(f"user:{login}", "packages")
-    if packages is None:
-        return render_template("dashboard.html", login=login, has_packages=False)
-
-    packages = json.loads(packages)
-    return render_template("dashboard.html", login=login, packages=packages, has_packages=True)
-
+    if not api_response_data["has_packages"]:
+        client_response = make_response(render_template("dashboard.html",
+             login=api_response_data["login"], has_packages=False))
+    else:
+        client_response = make_response(render_template("dashboard.html",
+             login=api_response_data["login"], packages=api_response_data["packages"], has_packages=True))
+    client_response.headers.set('links', links)
+    client_response.set_cookie('auth', cookie, max_age=20)
+    return client_response
 
 @app.route("/sender/dashboard", methods=['POST'])
 def add_package():
@@ -165,24 +121,22 @@ def add_package():
          "storeroom_id": storeroom_id,
          "size": size}
     }
-    login = g.login
+    json = {
+        "package": package,
+        "cookie": request.cookies.get('auth')
+    }
+    url = WEB_SEVICE_URL + "/root/sender/dashboard"
+    requests.post(url=url, json=json)
 
-    packages = db.hget(f"user:{login}", "packages")
-    if packages is None:
-        package = json.dumps(package)
-        db.hset(f"user:{login}", "packages", package)
-        return open_dashboard()
-
-    packages = json.loads(packages)
-    new_packages = {**packages, **package}
-    new_packages = json.dumps(new_packages)
-    db.hset(f"user:{login}", "packages", new_packages)
     return open_dashboard()
 
 @app.route("/sender/dashboard/delete", methods=['POST'])
 def add_packages():
-    login = g.login
-    db.hdel(f"user:{login}", "packages")
+    
+    url = WEB_SEVICE_URL + "/root/sender/dashboard"
+    cookie = request.cookies.get('auth')
+    requests.delete(url=url, json=cookie)
+
     return open_dashboard()
 
 if __name__ == '__main__':
